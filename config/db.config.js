a22027da3252686b9667d782883568d5e665371f
Angelu Ferdinand Abas Garcia <200610313@my.xu.edu.ph>
2020-08-06 17:46:49 +0800

    updated gitignore

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: "",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB,
 };
