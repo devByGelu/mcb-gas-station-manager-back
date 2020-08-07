62334e1d976c187c49f1befadfff0b379b0f3589
Angelu Ferdinand Abas Garcia <200610313@my.xu.edu.ph>
2020-08-07 18:41:57 +0800

    implement user password encryption

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,14 +2,20 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const sql = require("../models/db");
 const Employee = require("../models/Employee");
-
-exports.registerUser = async (req, res, next) => {
+const bcrypt = require("bcryptjs");
+exports.auth = async (req, res, next) => {
   try {
     const { uName, password, lName, mInit, fName, nickName } = req.body;
-    const token = jwt.sign({ id: uName }, process.env.JWT_SECRET, {
-      expiresIn: 3600,
-    });
-    const user = new User({ uName, password });
+    const token = jwt.sign(
+      { uName, password, lName, mInit, nickName },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 3600,
+      }
+    );
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = bcrypt.hash(password, salt);
+    const user = new User({ uName, password: encryptedPassword });
     await User.create(user);
     const employee = await Employee.create({
       lName,
